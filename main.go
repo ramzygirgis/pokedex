ac8d2c42c891997f5a53fd2c0611c3b68b30ebf5
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-04-08 10:22:27 -0400

    halfway done repl loop

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
 import "fmt"
+import "bufio"
+import "os"
+
+prompt := "Pokedex"
 
 func main() {
-	fmt.Println("Hello, World!")	
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(prompt + ">")
+		if scanner.Scan() {
+			token := scanner.Text() // might need to "forward declare" token
+		}
+
+
+	}
 }
 
