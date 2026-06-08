e733f242b977fa6f78ff0d3cb304cebc0ddba8f1
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-06-08 14:04:54 -0400

    modified types

--- internal/pokeapi/types.go
+++ internal/pokeapi/types.go
@@ -26,43 +26,148 @@ type Pokemon struct {
 	Id int `json:"id"`
 	Name string `json:"name"`
 	BaseExperience int `json:"base_experience"`
+	Height int `json:"height"`
+	Is_Default bool `json:"is_default"`
+	Weight int `json:"weight"`
 }
 
 type NamedAPIResource struct {
 	Name string `json:"name"`
 	URL  string `json:"url"`
 }
-
+ 
 type EncounterMethodRate struct {
 	EncounterMethod NamedAPIResource `json:"encounter_method"`
 	VersionDetails  []VersionDetail  `json:"version_details"`
 }
-
+ 
 type VersionDetail struct {
 	Rate    int              `json:"rate"`
 	Version NamedAPIResource `json:"version"`
 }
-
+ 
 type LocalizedName struct {
 	Language NamedAPIResource `json:"language"`
 	Name     string           `json:"name"`
 }
-
+ 
 type PokemonEncounter struct {
 	Pokemon        NamedAPIResource       `json:"pokemon"`
 	VersionDetails []VersionEncounterInfo `json:"version_details"`
 }
-
+ 
 type VersionEncounterInfo struct {
 	EncounterDetails []EncounterDetail `json:"encounter_details"`
 	MaxChance        int               `json:"max_chance"`
 	Version          NamedAPIResource  `json:"version"`
 }
-
+ 
 type EncounterDetail struct {
-	Chance         int                `json:"chance"`
+	Chance          int                `json:"chance"`
 	ConditionValues []NamedAPIResource `json:"condition_values"`
-	MaxLevel       int                `json:"max_level"`
-	Method         NamedAPIResource   `json:"method"`
-	MinLevel       int                `json:"min_level"`
+	MaxLevel        int                `json:"max_level"`
+	Method          NamedAPIResource   `json:"method"`
+	MinLevel        int                `json:"min_level"`
 }
+ 
+type Pokemon struct {
+	ID                     int                  `json:"id"`
+	Name                   string               `json:"name"`
+	BaseExperience         int                  `json:"base_experience"`
+	Height                 int                  `json:"height"`
+	IsDefault              bool                 `json:"is_default"`
+	Order                  int                  `json:"order"`
+	Weight                 int                  `json:"weight"`
+	Abilities              []PokemonAbility     `json:"abilities"`
+	Forms                  []NamedAPIResource   `json:"forms"`
+	GameIndices            []VersionGameIndex   `json:"game_indices"`
+	HeldItems              []PokemonHeldItem    `json:"held_items"`
+	LocationAreaEncounters string               `json:"location_area_encounters"`
+	Moves                  []PokemonMove        `json:"moves"`
+	PastTypes              []PokemonTypePast    `json:"past_types"`
+	PastAbilities          []PokemonAbilityPast `json:"past_abilities"`
+	PastStats              []PokemonStatPast    `json:"past_stats"`
+	Sprites                PokemonSprites       `json:"sprites"`
+	Cries                  PokemonCries         `json:"cries"`
+	Species                NamedAPIResource     `json:"species"`
+	Stats                  []PokemonStat        `json:"stats"`
+	Types                  []PokemonType        `json:"types"`
+}
+ 
+type PokemonAbility struct {
+	IsHidden bool             `json:"is_hidden"`
+	Slot     int              `json:"slot"`
+	Ability  NamedAPIResource `json:"ability"`
+}
+ 
+type VersionGameIndex struct {
+	GameIndex int              `json:"game_index"`
+	Version   NamedAPIResource `json:"version"`
+}
+ 
+type PokemonHeldItem struct {
+	Item           NamedAPIResource         `json:"item"`
+	VersionDetails []PokemonHeldItemVersion `json:"version_details"`
+}
+ 
+type PokemonHeldItemVersion struct {
+	Version NamedAPIResource `json:"version"`
+	Rarity  int              `json:"rarity"`
+}
+ 
+type PokemonMove struct {
+	Move                NamedAPIResource     `json:"move"`
+	VersionGroupDetails []PokemonMoveVersion `json:"version_group_details"`
+}
+ 
+type PokemonMoveVersion struct {
+	MoveLearnMethod NamedAPIResource `json:"move_learn_method"`
+	VersionGroup    NamedAPIResource `json:"version_group"`
+	LevelLearnedAt  int              `json:"level_learned_at"`
+	Order           int              `json:"order"`
+}
+ 
+type PokemonType struct {
+	Slot int              `json:"slot"`
+	Type NamedAPIResource `json:"type"`
+}
+ 
+type PokemonTypePast struct {
+	Generation NamedAPIResource `json:"generation"`
+	Types      []PokemonType    `json:"types"`
+}
+ 
+type PokemonAbilityPast struct {
+	Generation NamedAPIResource `json:"generation"`
+	Abilities  []PokemonAbility `json:"abilities"`
+}
+ 
+type PokemonStat struct {
+	Stat     NamedAPIResource `json:"stat"`
+	Effort   int              `json:"effort"`
+	BaseStat int              `json:"base_stat"`
+}
+ 
+type PokemonStatPast struct {
+	Generation NamedAPIResource `json:"generation"`
+	Stats      []PokemonStat    `json:"stats"`
+}
+ 
+type PokemonCries struct {
+	Latest string `json:"latest"`
+	Legacy string `json:"legacy"`
+}
+ 
+type PokemonSprites struct {
+	FrontDefault     string                 `json:"front_default"`
+	FrontShiny       string                 `json:"front_shiny"`
+	FrontFemale      string                 `json:"front_female"`
+	FrontShinyFemale string                 `json:"front_shiny_female"`
+	BackDefault      string                 `json:"back_default"`
+	BackShiny        string                 `json:"back_shiny"`
+	BackFemale       string                 `json:"back_female"`
+	BackShinyFemale  string                 `json:"back_shiny_female"`
+	Other            map[string]interface{} `json:"other"`
+	Versions         map[string]interface{} `json:"versions"`
+}
+ 
